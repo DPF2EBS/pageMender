c815e55166cfd3c4d0450ab7b51c5833743c78b6
xinwei.wang <wxwdesign@gmail.com>
2012-10-10 10:07:33 +0800

    update dom.js,add data status check

--- scripts/components/dom.js
+++ scripts/components/dom.js
@@ -5,10 +5,14 @@ define(function(require, exports, module) {
 		win.subMenu.eq(index).click(function(){
 			chrome.extension.sendMessage({from:'devtools',get:['nodeTags','nodeLength']}, function(response) {
 				var html=[];
-	
-				html.push('<h3>节点数=&gt;'+response['nodeLength']+'</h3>');
-				for(var key in response['nodeTags']){
-  					html.push('<a class="button dom-tags">'+key+'<sup class="red-dot">'+response['nodeTags'][key]+'</sup></a>');
+				
+				if(typeof response['nodeLength']!=='undefined'){
+					html.push('<h3>节点数=&gt;'+response['nodeLength']+'</h3>');
+					for(var key in response['nodeTags']){
+  						html.push('<a class="button dom-tags">'+key+'<sup class="red-dot">'+response['nodeTags'][key]+'</sup></a>');
+  					}
+  				}else{
+  					html.push('<h3>请刷新页面！</h3>');
   				}
 	
   				section.html(html.join(''));
