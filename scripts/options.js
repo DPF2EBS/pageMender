1f1b8a7c0d40c13517bfb1e45dcb1cf81025eeae
xinwei.wang <wxwdesign@gmail.com>
2012-11-06 11:09:27 +0800

    update options

--- scripts/options.js
+++ scripts/options.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
-	var li=$('.item-lists>li'),
-		section=$('article>section');
+	var featureSet=$('#features'),
+		featuresHTML=[],
+		configData=config.getData();
 
-	li.each(function(i){
-		$(this).click(function(){
-			li.removeClass('active');
-			$(this).addClass('active');
-			section.slideUp();
-			section.eq(i).slideDown();
+	config.data.forEach(function(dataGroup, index){
+		/*<fieldset>
+            <legend>页面分析</legend>
+            <ul>
+                <li>
+                    <label for="">DOM</label>
+                    <input type="checkbox" name="" id="">
+                </li>
+            </ul>    
+        </fieldset>*/
+		featuresHTML.push('<fieldset><legend>'+dataGroup.name+'</legend><ul>');
+		dataGroup.submenu.forEach(function(f,fIndex){
+			featuresHTML.push('<li><label for="">'+f.name+'</label><input type="checkbox" name="" id=""></li>');
 		});
+		featuresHTML.push('</ul></fieldset>');
 	});
+
+	console.log(featuresHTML);
 });
